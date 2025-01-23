cf64f2333c9022177015956a41ff288187509011
Michał Napora <napora.michael@gmail.com>
2025-01-23 11:50:15 +0100

    Added messages for non valid fields

--- service-shuffle/src/main/java/bgr/matrixee/shuffle/presentation/ShuffleRequestBody.java
+++ service-shuffle/src/main/java/bgr/matrixee/shuffle/presentation/ShuffleRequestBody.java
@@ -5,8 +5,8 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 record ShuffleRequestBody(
-        @NotNull
-        @Min(1)
-        @Max(1000)
+        @NotNull(message = "The numbersToShuffleCount field must not be null.")
+        @Min(value = 1, message = "The numbersToShuffleCount field must be at least 1.")
+        @Max(value = 1000, message = "The numbersToShuffleCount field must not exceed 1000.")
         Integer numbersToShuffleCount
 ) {}
