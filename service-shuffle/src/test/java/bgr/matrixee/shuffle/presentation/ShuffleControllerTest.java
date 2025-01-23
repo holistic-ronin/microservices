2553ec524df82f067b03cc4b424287068c534b8b
Michał Napora <napora.michael@gmail.com>
2025-01-23 07:46:14 +0100

    Fixed test

--- service-shuffle/src/test/java/bgr/matrixee/shuffle/presentation/ShuffleControllerTest.java
+++ service-shuffle/src/test/java/bgr/matrixee/shuffle/presentation/ShuffleControllerTest.java
@@ -1,28 +1,29 @@
 package bgr.matrixee.shuffle.presentation;
 
-import bgr.matrixee.shuffle.domain.ShuffleService;
+import bgr.matrixee.shuffle.domain.ShuffleFacade;
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class ShuffleControllerTest {
 
-    private final ShuffleService service = mock(ShuffleService.class);
-    private final ShuffleController controller = new ShuffleController(service);
+    private final ShuffleFacade facade = mock(ShuffleFacade.class);
+    private final HttpServletRequest servletRequest = mock(HttpServletRequest.class);
+    private final ShuffleController controller = new ShuffleController(facade);
 
     @Test
     void shouldReturnShuffledResponse() {
         //given:
         final var numberToShuffle = 5;
-        final var request = new ShuffleRequestBody(numberToShuffle);
+        final var requestBody = new ShuffleRequestBody(numberToShuffle);
         final var shuffledArray = new int[]{4, 2, 1, 5, 3};
-        when(service.createAndShuffleArray(numberToShuffle)).thenReturn(shuffledArray);
+        when(facade.shuffleArray(servletRequest, numberToShuffle)).thenReturn(shuffledArray);
 
         //when:
-        final var response = controller.shuffleNumbers(request);
+        final var response = controller.shuffleNumbers(requestBody, servletRequest);
 
         //then:
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
