6d35cf23615d910311c8d2c5dace74a1e5c7f8f1
Michał Napora <napora.michael@gmail.com>
2025-01-23 05:25:46 +0100

    Modified import

--- service-log/src/main/java/bgr/matrixee/log/domain/ConsoleLogger.java
+++ service-log/src/main/java/bgr/matrixee/log/domain/ConsoleLogger.java
@@ -1,10 +1,11 @@
 package bgr.matrixee.log.domain;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 class ConsoleLogger implements Logger {
-    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(ConsoleLogger.class);
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(ConsoleLogger.class);
 
     @Override
     public void log(final String message) {
